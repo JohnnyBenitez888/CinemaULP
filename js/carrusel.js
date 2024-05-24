59de1186c2bc326625708c9b9e5d099466218a4f
Johnny Benitez <johnnybeniitez@gmail.com>
2024-05-24 02:20:52 -0300

    Se corrigió el nombre de las funciones anterior() y siguiente()

--- js/carrusel.js
+++ js/carrusel.js
@@ -3,45 +3,44 @@
 let i = 0;
 let imagenes = [
     "assets/img/aclamada_1.jpg",
- "assets/img/aclamada_2.jpg", 
- "assets/img/aclamada_3.jpg",
- "assets/img/aclamada_4.jpg",
- "assets/img/aclamada_5.jpg",
- "assets/img/aclamada_6.jpg",
- "assets/img/aclamada_7.jpg",
- "assets/img/aclamada_8.jpg",
- "assets/img/aclamada_9.jpg",
- "assets/img/aclamada_10.jpg",
- "assets/img/aclamada_11.jpg",
- "assets/img/aclamada_12.jpg",
- "assets/img/aclamada_13.jpg",
- "assets/img/aclamada_14.jpg",
- "assets/img/aclamada_15.jpg",
- "assets/img/aclamada_16.jpg",
- "assets/img/aclamada_17.jpg",
- "assets/img/aclamada_18.jpg",];
+    "assets/img/aclamada_2.jpg",
+    "assets/img/aclamada_3.jpg",
+    "assets/img/aclamada_4.jpg",
+    "assets/img/aclamada_5.jpg",
+    "assets/img/aclamada_6.jpg",
+    "assets/img/aclamada_7.jpg",
+    "assets/img/aclamada_8.jpg",
+    "assets/img/aclamada_9.jpg",
+    "assets/img/aclamada_10.jpg",
+    "assets/img/aclamada_11.jpg",
+    "assets/img/aclamada_12.jpg",
+    "assets/img/aclamada_13.jpg",
+    "assets/img/aclamada_14.jpg",
+    "assets/img/aclamada_15.jpg",
+    "assets/img/aclamada_16.jpg",
+    "assets/img/aclamada_17.jpg",
+    "assets/img/aclamada_18.jpg",];
 
- function cambiarImagen(){
+function cambiarImagen() {
     let img = document.getElementById("img-aclamada");
     img.src = imagenes[i];
- }
+}
 
- function anterior(){
-    if(i>0){
+function anterior() {
+    if (i > 0) {
         i--;
-    }else{
+    } else {
         i = imagenes.length - 1;
     }
     cambiarImagen();
- }
+}
 
- function siguiente(){
-    if(i<imagenes.length -1){
+function siguiente() {
+    if (i < imagenes.length - 1) {
         i++;
-    }else{
+    } else {
         i = 0;
     }
     cambiarImagen();
- }
+}
 
- 
