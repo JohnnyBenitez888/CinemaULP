b90119b9139c5d249a6b200c2c410740841e0b5d
Johnny Benitez <johnnybeniitez@gmail.com>
2024-05-25 00:42:26 -0300

    *

--- js/registrarse.js
+++ js/registrarse.js
@@ -1,3 +1,12 @@
-function ocultar(){
+/* function ocultar(){
     document.getElementById("oculto").style.display = "none";
+} */
+
+function validateForm(){
+
 }
+
+let parrafo = document.createElement("p");
+parrafo.innerHTML = "Hola Mundo";
+
+document.getElementById("").appendChild(parrafo);
